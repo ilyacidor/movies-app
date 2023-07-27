954d207df064b4d7c77f40caf57942147fa6c5e6
Ilya Sidorov <114821505+ilyacidor@users.noreply.github.com>
2023-07-27 19:31:49 +0300

    Add files via upload

---
+++ src/components/app/app.js
@@ -0,0 +1,221 @@
+import React, { Component } from 'react'
+import { format } from 'date-fns'
+import { Spin, Alert, Pagination, Tabs } from 'antd'
+import { Offline, Online } from 'react-detect-offline'
+
+import SearchBar from '../search-bar'
+import ItemList from '../item-list'
+import GetResource from '../../get-resource'
+import GenreContext from '../../genre-context'
+import './app.css'
+
+export default class App extends Component {
+  state = {
+    movieData: [],
+    genresData: [],
+    loading: true,
+    error: false,
+    activeKey: '1',
+    currentPage: 1,
+    totalResults: 0,
+    query: '',
+  }
+
+  getResource = new GetResource()
+
+  componentDidMount() {
+    this.loadGenres()
+  }
+
+  componentDidUpdate(prevState) {
+    if (
+      (this.state.currentPage !== prevState.currentPage ||
+        this.state.query !== prevState.query ||
+        this.state.activeKey !== prevState.activeKey) &&
+      this.state.movieData.length === 0
+    ) {
+      this.updateMovieData()
+    }
+  }
+
+  componentDidCatch() {
+    this.setState({ error: true })
+  }
+
+  shortText = (text, index) => {
+    let shortenedText = text.slice(0, index)
+    let lastLetter = index - 1
+    while (shortenedText[lastLetter] !== ' ') {
+      shortenedText = shortenedText.slice(0, lastLetter)
+      lastLetter--
+    }
+    shortenedText += ' ...'
+    return shortenedText
+  }
+
+  shortTitle = (title) => {
+    title = title.length > 26 ? this.shortText(title, 26) : title
+    return title
+  }
+
+  shortOverview = (text, title) => {
+    if (title.length > 13 && text.length > 160) {
+      text = this.shortText(text, 160)
+    }
+    if (title.length <= 13 && text.length > 200) {
+      text = this.shortText(text, 200)
+    }
+    return text
+  }
+
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
+    })
+  }
+
+  loadGenres() {
+    this.getResource.getGenres().then((obj) => {
+      obj.genres.forEach((genre) => {
+        this.setState(({ genresData }) => {
+          const newItem = {
+            genreId: genre.id,
+            genreName: genre.name,
+          }
+          const newArray = [...genresData, newItem]
+          return {
+            genresData: newArray,
+          }
+        })
+      })
+    })
+  }
+
+  updateMovieData = () => {
+    this.getResource
+      .getMovies(this.state.currentPage, this.state.query)
+      .then((movies) => {
+        if (this.state.activeKey == 1) {
+          this.setState({ totalResults: movies.total_results })
+        } else {
+          if (localStorage.length < this.state.currentPage * 20) {
+            this.setState({ totalResults: this.state.currentPage * 20 })
+          } else {
+            this.setState({ totalResults: localStorage.length })
+          }
+        }
+        movies.results.forEach((movie) => {
+          this.setState(
+            ({ movieData }) => {
+              let imgPath = 'https://movienewsletters.net/photos/000000h1.jpg'
+              if (movie.poster_path !== null) imgPath = 'https://image.tmdb.org/t/p/original' + movie.poster_path
+              let date = 'release date unknown'
+              if (movie.release_date.length === 10) date = format(new Date(movie.release_date), 'MMMM dd, yyyy')
+              let rate = 0
+              if (localStorage.getItem(movie.id) !== null || localStorage.getItem(movie.id) > 0)
+                rate = Number(localStorage.getItem(movie.id))
+              const newItem = {
+                id: movie.id,
+                posterPath: imgPath,
+                title: this.shortTitle(movie.title),
+                voteAverage: Math.round(Number(movie.vote_average) * 10) / 10,
+                releaseDate: date,
+                genreIds: movie.genre_ids,
+                overview: this.shortOverview(movie.overview, movie.title),
+                userRate: rate,
+              }
+              const newArray = [...movieData, newItem]
+              return {
+                movieData: newArray,
+              }
+            },
+            () => {}
+          )
+        })
+        this.setState({ loading: false })
+      })
+      .catch(this.onError)
+  }
+
+  updateQuery = (q) => {
+    this.setState({ currentPage: 1, query: q, movieData: [], loading: true })
+  }
+
+  updatePage = (page) => {
+    this.setState({ currentPage: page, movieData: [], loading: true })
+  }
+
+  changeTab = (key) => {
+    this.setState({ activeKey: key, currentPage: 1, query: '', movieData: [], loading: true })
+  }
+
+  filterRatedMovies = () => {
+    const { movieData, activeKey } = this.state
+    if (activeKey == 2) {
+      return movieData.filter(({ userRate }) => userRate > 0)
+    }
+    return movieData
+  }
+
+  render() {
+    const { movieData, genresData, loading, error, currentPage, totalResults, query, activeKey } = this.state
+    const items = [
+      {
+        key: '1',
+        label: 'Search',
+      },
+      {
+        key: '2',
+        label: 'Rated',
+      },
+    ]
+    const errorMessage = error ? (
+      <Alert
+        message="Boom!"
+        description="Something has gone terribly wrong! (But we are already trying to fix it)"
+        type="error"
+      />
+    ) : null
+    if (error) return errorMessage
+    const spin = loading ? (
+      <Spin tip="Loading" size="large">
+        <div className="content" />
+      </Spin>
+    ) : null
+    const notFound =
+      movieData.length === 0 && !(loading || error) ? (
+        <Alert message="Oops!" description="Movies Not Found" type="info" />
+      ) : null
+    const content = !(loading || error) ? (
+      <div>
+        {notFound}
+        <GenreContext.Provider value={genresData}>
+          <ItemList movies={this.filterRatedMovies()} />
+        </GenreContext.Provider>
+        <Pagination
+          current={currentPage}
+          total={totalResults}
+          pageSize={20}
+          pageSizeOptions={[20]}
+          onChange={this.updatePage}
+        />
+      </div>
+    ) : null
+
+    return (
+      <div className="container">
+        <Online>
+          <Tabs defaultActiveKey="1" centered items={items} onChange={this.changeTab} destroyInactiveTabPane="true" />
+          <SearchBar updateMovieList={this.updateQuery} query={query} activeKey={activeKey} />
+          {spin}
+          {content}
+          {errorMessage}
+        </Online>
+        <Offline>
+          <Alert message="No connection!" description="Please check your internet connection!" type="error" />
+        </Offline>
+      </div>
+    )
+  }
+}
---
+++ src/components/movie-item/movie-item.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react'
+import { Rate } from 'antd'
+
+import GenreContext from '../../genre-context'
+import GenreItem from '../genre-item'
+import './movie-item.css'
+
+const MovieItem = ({ id, posterPath, title, releaseDate, overview, userRate, genreIds, voteAverage }) => {
+  const changeRating = (value) => {
+    localStorage.setItem(id, value)
+  }
+  let genres = useContext(GenreContext)
+  if (genreIds.length > 2) {
+    genreIds = genreIds.slice(0, 2)
+  }
+  let arr = []
+  genres.forEach((element) => {
+    genreIds.forEach((el) => {
+      if (element.genreId == el) arr.push(element)
+    })
+  })
+  const elements = arr.map((item) => {
+    const { ...itemProps } = item
+    const { genreId } = itemProps
+    return <GenreItem key={genreId} {...itemProps} />
+  })
+  let voteClass = 'vote-average vote-average--'
+  let color = ''
+  color = voteAverage < 3 ? 'red' : voteAverage < 5 ? 'orange' : voteAverage < 7 ? 'yellow' : 'green'
+  voteClass += color
+  voteAverage = String(voteAverage).length < 2 ? (voteAverage += '.0') : voteAverage
+  return (
+    <div className="movie-item">
+      <div className="poster-container">
+        <img src={posterPath} alt="poster" className="poster"></img>
+      </div>
+      <section className="movie">
+        <h2 className="movie__title">{title}</h2>
+        <p className="movie__date">{releaseDate}</p>
+        <div className="movie__genres">{elements}</div>
+        <p className="movie__overview">{overview}</p>
+        <Rate allowHalf defaultValue={userRate} count={10} className="user-rate" onChange={changeRating} />
+      </section>
+      <span className={voteClass}>{voteAverage}</span>
+    </div>
+  )
+}
+
+export default MovieItem
